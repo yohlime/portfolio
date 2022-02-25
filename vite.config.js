9fd3133264fc98ed5ea8bebb0c79b46d996fa5da
Emilio Gozo <emiliogozo@pm.me>
2022-02-25 23:30:00 +0800

    fix: build outDir

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [react(), svgr()]
 });
