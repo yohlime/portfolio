92dd7fce2e58726663ceff8de0d2c54d5b1193fc
Emilio Gozo <15209351+emiliogozo@users.noreply.github.com>
2024-01-27 22:50:19 +0800

    chore: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from 'astro/config'
 
-import vue from "@astrojs/vue";
+import netlify from '@astrojs/netlify'
+import vue from '@astrojs/vue'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS({
-    injectReset: true
-  }), vue()]
-});
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    vue(),
+  ],
+  output: 'server',
+  adapter: netlify(),
+})
