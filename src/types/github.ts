c0817daadcc8a8e55d4f9e0160054467f4083b0c
Emilio Gozo <15209351+emiliogozo@users.noreply.github.com>
2024-01-27 13:26:14 +0800

    use codicons

--- src/types/github.ts
+++ src/types/github.ts
@@ -4,3 +4,4 @@ import { ghProfile } from '../schemas/github'
 
 type _GHProfile = z.infer<typeof ghProfile>
 export type GHProfile = _GHProfile['data']['user']
+export type GHRepo = GHProfile['pinnedItems']['edges'][0]['node']
