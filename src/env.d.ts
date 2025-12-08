d1bf898b1eb69764af1d270bf41071a91b39187a
Emilio Gozo <15209351+yohlime@users.noreply.github.com>
2025-12-08 23:03:32 +0800

    chore: migrate to cloudflare

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+type Runtime = import('@astrojs/cloudflare').Runtime<Env>;
+
+declare namespace App {
+  interface Locals extends Runtime {
+    otherLocals: {
+      VITE_ENVIRONMENT: string;
+    };
+  }
+}
