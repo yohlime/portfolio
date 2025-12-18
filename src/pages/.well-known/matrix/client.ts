6482fc2da84d7374a8d6f9ee00c3f0605c53d8d5
Emilio Gozo <15209351+yohlime@users.noreply.github.com>
2025-12-19 00:47:43 +0800

    feat: add matrix livekit

--- src/pages/.well-known/matrix/client.ts
+++ src/pages/.well-known/matrix/client.ts
@@ -5,7 +5,13 @@ export async function GET() {
     },
     "m.identity_server": {
       "base_url": "https://vector.im"
-    }
+    },
+    "org.matrix.msc4143.rtc_foci":[
+      {
+        "type":"livekit",
+        "livekit_service_url": "https://matrixrtc.yohli.me"
+      }
+    ]
   }
   return new Response(JSON.stringify(d), {
     status: 200,
